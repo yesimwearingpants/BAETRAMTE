bc9246acd437b1d2c68280ac5962f764be8b48de
yesimwearingpants <yesimwearingpants@gmail.com>
2015-02-24 18:11:16 -0500

    Minor Changes

--- src/main/java/com/yiwp/batmanplusplus/creativetab/BPPCreativeTab.java
+++ src/main/java/com/yiwp/batmanplusplus/creativetab/BPPCreativeTab.java
@@ -1,4 +1,4 @@
-/*
+/**
  * This file is part of Blue Power.
  *
  *     Blue Power is free software: you can redistribute it and/or modify
@@ -13,9 +13,13 @@
  *
  *     You should have received a copy of the GNU General Public License
  *     along with Blue Power.  If not, see <http://www.gnu.org/licenses/>
+ *     
+ *     Modified By <SomeNoNameModder> 
  */
 
 package com.yiwp.batmanplusplus.creativetab;
+
+import com.yiwp.batmanplusplus.lib.reference.Reference;
 
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
@@ -23,59 +27,55 @@ import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class BPPCreativeTab {
+public class BPPCreativeTab extends CreativeTabs{
 
-    public static CreativeTabs INSTANCE;
+	public static final BPPCreativeTab INSTANCE = new BPPCreativeTab(Reference.NAME, true);
+	private boolean search;
 
-    static {
+	public BPPCreativeTab(String label, boolean searchbar)
+	{
+		super(label);
+        this.search = searchbar;
+	}
+	
+	@Override
+    public boolean hasSearchBar() {
 
-        INSTANCE = new BPPCreativeTabs("tabbatman", true) {
-
-            @Override
-            @SideOnly(Side.CLIENT)
-            public Item getTabIconItem() {
-                return null;
-            }
-
-            @Override
-            @SideOnly(Side.CLIENT)
-            public ItemStack getIconItemStack() {
-            	return new ItemStack(Blocks.stone);
-            }
-        };
+        return search;
     }
 
-    private static abstract class BPPCreativeTabs extends CreativeTabs {
+	@Override
+	public ItemStack getIconItemStack()
+	{
+		return new ItemStack(Blocks.acacia_stairs, 1, 16);
+	}
 
-        private boolean searchbar = false;
+	@Override
+	public String getTranslatedTabLabel()
+	{
+		return this.getTabLabel();
+	}
+	
+	@Override
+    @SideOnly(Side.CLIENT)
+    public String getBackgroundImageName() {
 
-        public BPPCreativeTabs(String label, boolean searchbar) {
-        	super(label);
-            this.searchbar = searchbar;
-        }
-
-        @Override
-        public boolean hasSearchBar() {
-
-            return searchbar;
-        }
-
-        @Override
-        @SideOnly(Side.CLIENT)
-        public String getBackgroundImageName() {
-
-            return searchbar ? "item_search.png" : super.getBackgroundImageName();
-        }
-
-        @Override
-        public int getSearchbarWidth() {
-
-            return 62;
-        }
-
+        return search ? "item_search.png" : super.getBackgroundImageName();
     }
+
+    @Override
+    public int getSearchbarWidth() {
+
+        return 62;
+    }
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Item getTabIconItem()
+	{
+		return null;
+	}
 }
