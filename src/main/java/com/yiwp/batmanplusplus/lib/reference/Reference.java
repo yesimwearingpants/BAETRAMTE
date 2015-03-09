198b4675d24321bf810c4635e8d06b0fd06c2364
yesimwearingpants <yesimwearingpants@gmail.com>
2015-03-09 00:32:14 -0400

    A Commit a Day...

--- src/main/java/com/yiwp/batmanplusplus/lib/reference/Reference.java
+++ src/main/java/com/yiwp/batmanplusplus/lib/reference/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
     public static final String NAME = "Batman++";
     public static final String MODID = "batmanplusplus";
     public static final String VERSION = "1.7.10-" + "@VERSION@";
-    public static final String BUILD = "0";
+    public static final String BUILD = "@BUILD@";
     public static final String INSTANCE = MODID;
     public static final String CLIENT = "com.yiwp.batmanplusplus.proxy.BPPProxyClient";
     public static final String SERVER = "com.yiwp.batmanplusplus.proxy.BPPProxyServer";
