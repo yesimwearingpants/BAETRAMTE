25784afe61336e52e514a7e395201a01f0b2799a
yesimwearingpants <yesimwearingpants@gmail.com>
2015-03-15 23:55:31 -0400

    ?

--- src/main/java/com/yiwp/batmanplusplus/proxy/BPPProxyClient.java
+++ src/main/java/com/yiwp/batmanplusplus/proxy/BPPProxyClient.java
@@ -1,7 +1,5 @@
 package com.yiwp.batmanplusplus.proxy;
 
-import com.yiwp.batmanplusplus.client.render.LightRender;
-
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -9,15 +7,15 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class BPPProxyClient extends BPPProxyCommon {
 	
-	public static int renderPass;	
-	public static int lightRender;
+	//public static int renderPass;	
+	//public static int lightRender;
 	
        
 	@Override
 	public void registerRenderers() {
 		
-		lightRender = RenderingRegistry.getNextAvailableRenderId();
-		RenderingRegistry.registerBlockHandler(new LightRender());
+		//lightRender = RenderingRegistry.getNextAvailableRenderId();
+		//RenderingRegistry.registerBlockHandler(new LightRender());
 	}
 
 }
